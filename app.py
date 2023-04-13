95395edf2603b778f20d13d1cdb10149eb0f0608
utksh <utksh09@gmail.com>
2023-04-13 23:54:27 +0530

    running app

--- app.py
+++ app.py
@@ -32,78 +32,26 @@ with open('trained_model.pkl', 'wb') as f:
 # create a Flask app
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, World!"
-
-def on_request(ch, method, props, body):
+# define a route for the API
+@app.route('/predict', methods=['POST'])
+def predict():
     # load the saved model
     with open('trained_model.pkl', 'rb') as f:
         model = pickle.load(f)
-
-    # parse the message body
-    message = json.loads(body)
-    description = message['description']
-
+    
+    # get the request data
+    data = request.get_json()
+    description = data['description']
+    
     # transform the description using the vectorizer
     description_transformed = tfidf.transform([description])
-
+    
     # make a prediction using the loaded model
     prediction = model.predict(description_transformed)[0]
-
-    # create a response message
+    
+    # return the prediction as a JSON response
     response = {'prediction': prediction}
-
-    # send the response message back to the sender
-    ch.basic_publish(
-        exchange='',
-        routing_key=props.reply_to,
-        properties=pika.BasicProperties(
-            correlation_id=props.correlation_id
-        ),
-        body=json.dumps(response)
-    )
-
-    # acknowledge the message
-    ch.basic_ack(delivery_tag=method.delivery_tag)
-
-@app.route("/predict", methods=["POST"])
-def predict():
-    # create a connection to the RabbitMQ server
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    channel = connection.channel()
-
-    # create a queue to receive messages
-    channel.queue_declare(queue='prediction_queue')
-
-    # generate a unique correlation ID
-    correlation_id = str(uuid.uuid4())
-
-    # create a message payload
-    payload = {'description': request.json['description']}
-
-    # send the message to the prediction queue
-    channel.basic_publish(
-        exchange='',
-        routing_key='prediction_queue',
-        properties=pika.BasicProperties(
-            reply_to='prediction_response',
-            correlation_id=correlation_id
-        ),
-        body=json.dumps(payload)
-    )
-
-    # set up a listener to receive the prediction response
-    channel.basic_consume(
-        queue='prediction_response',
-        on_message_callback=on_request
-    )
-
-    # start consuming messages
-    channel.start_consuming()
-
-    # close the connection
-    connection.close()
+    return jsonify(str(response))
 
 if __name__ == '__main__':
     app.run()
